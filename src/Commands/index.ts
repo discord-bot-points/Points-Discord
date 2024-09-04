4f8180623b24eb3c9dd588ed35fecfc72ece5922
evarellapucky <scherer.alexia@gmail.com>
2024-09-04 09:37:38 +0200

    clean commands

--- src/Commands/index.ts
+++ src/Commands/index.ts
@@ -1,8 +1,6 @@
 import * as ping from "./ping";
-import * as tic from "./tic";
-import * as pouet from "./pouet";
 import * as balance from "./balance";
 
 export const commands = {
-  ping, tic, pouet, balance,
+  ping, balance,
 };
